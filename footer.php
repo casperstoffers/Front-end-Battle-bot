7b7e62d44f5011fee244f3ba06b967578e8cd98d
MSI\Tho09 <thomas.fluit@student.stenden.com>
2018-03-07 17:17:34 +0200

    consept website

---
+++ footer.php
@@ -0,0 +1,8 @@
+<div id="footer">
+    <div id="footerLeft">
+        <a href="disclaimer.php"><p>disclaimer</p>
+    </div>
+    <div id="footerRight">
+        <a href="cookies.php"><p>cookies</p></a>
+    </div>
+</div>
---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <title> Arduino Bots </title>
+        <link href="stylesheet.css" type="text/css" rel="stylesheet" />
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <?php
+            include 'header.php';
+            include 'contentlogin.php';
+            include 'footer.php';
+        ?>
+    </body>
+</html>
