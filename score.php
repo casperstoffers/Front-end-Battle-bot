c3b8e683f0e4fee26ef945d6fed5c576c904ea1a
axel <axelkrak95@gmail.com>
2018-04-03 14:15:52 +0200

    scoreboard 3-4-2018

---
+++ score.php
@@ -0,0 +1,173 @@
+<?PHP
+		$score_1 = "0";
+		if(isset($_POST["submit"])){
+			
+				
+			$score_1 = $_POST["score"];
+			$bot = $_POST["bot_id"];
+			$ronde = $_POST["ronde_1"];
+			
+			
+				if (!empty($_POST["score"])){
+					
+																		$db = mysqli_connect("localhost", "root", "", "scoreboard");
+																		if ($db == true)
+																			{
+																				echo "database connectie gemaakt<br>";
+																				
+																				echo "en je hebt een score op bot numeer " . $bot ." ingevoerd van  " . $score_1 . " <br>";
+																				
+																				$sql = "update score set score_". $ronde  ." = ". $score_1 ." where score.bot_id = "  . $bot ."";
+																				
+																				$result = mysqli_query($db,$sql);
+																						
+																						if($sql == true)
+																						
+																						{
+																							echo "gegevens in gevoerd";
+																							header ("refresh:1, url=score.php");
+																							echo "Error: " . $sql . "<br>" . mysqli_error($db);
+																						}else 
+																								{
+																									echo "Error: " . $sql . "<br>" . mysqli_error($db);
+																								}
+																			}else
+																						{
+																							echo "geen database connectie kunnen maken";
+																						}
+				}else 
+					{
+						echo "vul score in";
+					}
+		}
+	
+?>
+
+
+
+<!DOCTYPE HTML>
+<html>
+<head>
+
+</head>
+<body>
+<form action="score.php" method="POST">
+  <div class="form-group">
+    <label for="exampleInputEmail1">score</label>
+    <input type="number" class="form-control"  name="score" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    
+  </div>
+  
+  
+  
+<div class="form-group">
+    <label for="exampleFormControlSelect1">set score</label>
+    <select name="ronde_1" class="form-control" id="exampleFormControlSelect1">
+				  <option value="1">ronde 1</option>
+				  <option value="2">ronde 2</option>
+				  <option value="3">ronde 3</option>
+				  <option value="4">ronde 4</option>
+			</select>
+			</div>
+			
+			<div class="form-group">
+    <label for="exampleFormControlSelect1">selecet a bot</label>
+    <select name="bot_id" class="form-control" id="exampleFormControlSelect1">
+      
+				  <option value="1">bot 1</option>
+				  <option value="2">bot 2</option>
+				  <option value="3">bot 3</option>
+				  <option value="4">bot 4</option>
+				  <option value="5">bot 5</option>
+				  <option value="6">bot 6</option>
+				  <option value="7">bot 7</option>
+				  <option value="8">bot 8</option>
+				  <option value="9">bot 9</option>
+				  <option value="10">bot 10</option>
+			</select>
+			</div>
+			
+<button type="submit" name="submit" class="btn btn-primary">Submit</button>
+
+
+</form>
+
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<div class="container">
+    
+    	<div class="row">
+			<div class="col-md-6">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<h3 class="panel-title">score's</h3>
+						<div class="pull-right">
+							
+						</div>
+					</div>
+					<div class="panel-body">
+						
+					</div>
+					<table class="table table-hover" id="dev-table">
+						<thead>
+							<tr>
+								<th> bot id#</th>
+								<th>score 1</th>
+								<th>score 2</th>
+								<th>score 3</th>
+								<th>score 4</th>
+								<th>Total score</th>
+								
+								
+							</tr>
+						</thead>
+						
+								<?php
+
+$db = mysqli_connect("localhost", "root", "", "scoreboard");
+									
+										
+										$select = "select * from score where score_1 >= 0 order by total_score desc";
+										
+										$result_2 = mysqli_query($db, $select);
+										
+																	while ($row = mysqli_fetch_array($result_2))
+																				{
+																			echo "<tbody>
+							<tr>";
+																								
+																								echo "<td class='cell100 column2'>" . $row["bot_id"] . "</td> ";
+																								echo "<td class='cell100 column2'>" . $row["score_1"] . "</td> ";
+																								echo "<td class='cell100 column3'>" . $row["score_2"] . " </td>";
+																								echo "<td class='cell100 column4'>" . $row["score_3"] . "</td> ";
+																								echo "<td class='cell100 column5'>" . $row["score_4"] . "</td>";
+																								
+																								$total_score = $row["score_1"] + $row["score_2"] + $row["score_3"] + $row["score_4"];
+																								echo "<td class=cell100 column6> " . $total_score . "</td>";
+																								
+																							$sql2 = "update score set total_score = '". $total_score ."' where score.bot_id = '".$row["bot_id"]."'";
+																							$result2 = mysqli_query($db,$sql2);
+																							if($result2 == true)
+																						
+																								{
+																										
+																								}else 
+																										{
+																											echo "Error: " . $sql2 . "<br>" . mysqli_error($db);
+																										}
+																				}
+
+									
+?>
+							</tr>
+							
+						</tbody>
+					</table>
+				</div>
+			</div>
+			
+		</div>
+	</div>
