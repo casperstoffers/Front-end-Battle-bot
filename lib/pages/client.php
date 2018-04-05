d453c8a1cf390759d452a04f40c52c73b2d09d38
Casper Stoffers <casperstoffers@gmail.com>
2018-04-05 12:10:56 +0200

    src aanpassing van controls image

--- lib/pages/client.php
+++ lib/pages/client.php
@@ -2,7 +2,7 @@
 		<div class="row">                  
 			<h1 class="text-center">Bestuur je battlebot!</h1>
                         <div class="col-md-6">
-                            <img  style="height: 300px; width: 400px; margin-top: 40px;"src="tpl/image/battlebotcontrols.jpg"/>
+                            <img  style="height: 300px; width: 400px; margin-top: 40px;"src="../../tpl/image/battlebotcontrols.jpg"/>
                             <p style=" margin-top: 40px;" id="control"></p>
                             <audio id="bigCar">
                                 <source src="lib/audio/formula+1.mp3">
@@ -20,13 +20,12 @@
                             if(event.keyCode == 87 || event.keyCode == 65 || event.keyCode == 68){
                                 document.getElementById('bigCar').play();
                             }
-                                                       
-                           
+                                                                                 
                             else if(event.keyCode == 81) {
-                                document.getElementById('control').innerHTML = "Special 1";
+                                //custom command 1
                             }
                             else if(event.keyCode == 69) {
-                                document.getElementById('control').innerHTML = "Special 2";
+                                //custom command 2
                             }
 
                             });
